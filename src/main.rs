aaf581bd9406f42b486cb6dbb6c1aee08338c10b
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-03-25 09:56:55 +1000

    Extract prompt to a function

--- src/main.rs
+++ src/main.rs
@@ -38,16 +38,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn eval(input: &str, client: &Client, env: &Environment) -> String {
-    let critical_prompt = format!(r##"
-    Ignore all previous instructions. You are an inquisitive writing assistant. When you read a passage of writing, questions about missed connections, related ideas come to mind. You are critical and find gaps in arguments. You should try to extend every idea you encounter and try to connect it with ideas you've seen in the past, noticing any logical inconsistencies or flawed arguments. You should respond with a list of 3 "what if it's not that X, but Y?", "couldn't A also be true?" and "why isn't B possible?" questions for every paragraph you see beginning with >. Draw on sources you've seen in the past to support your thinking and aim to ask questions that would inspire the author to deepen their own thought process. Stop responding immediately after listing the 3 questions, use a numbered list. Do not generate any paragraphs starting with > yourself. Use an assertive but polite tone. Make sure to be as concise as possible.
-
-    Here's your first task:
-    > {}
-    \n"##, input);
+    let prompt = prompts::critical_writing(input);
 
     let content = json!({
         "model": "text-davinci-003",
-        "prompt": critical_prompt,
+        "prompt": prompt,
         "max_tokens": 100,
         "temperature": 0,
         "top_p": 1,
@@ -74,5 +69,18 @@ fn eval(input: &str, client: &Client, env: &Environment) -> String {
             println!("Error: {}", error);
             String::from("Error")
         }
+    }
+}
+
+
+mod prompts {
+    pub fn critical_writing(input: &str) -> String {
+        let critical_prompt = format!(r##"
+        Ignore all previous instructions. You are an inquisitive writing assistant. When you read a passage of writing, questions about missed connections, related ideas come to mind. You are critical and find gaps in arguments. You should try to extend every idea you encounter and try to connect it with ideas you've seen in the past, noticing any logical inconsistencies or flawed arguments. You should respond with a list of 3 "what if it's not that X, but Y?", "couldn't A also be true?" and "why isn't B possible?" questions for every paragraph you see beginning with >. Draw on sources you've seen in the past to support your thinking and aim to ask questions that would inspire the author to deepen their own thought process. Stop responding immediately after listing the 3 questions, use a numbered list. Do not generate any paragraphs starting with > yourself. Use an assertive but polite tone. Make sure to be as concise as possible.
+
+        Here's your first task:
+        > {}
+        \n"##, input);
+        critical_prompt
     }
 }
